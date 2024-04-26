c1332af1268fca2f0ef1c2b44d52d7e094ca9f04
Vasiliy <84288491+Pollultr@users.noreply.github.com>
2024-04-26 21:45:47 +0300

    Create HWW3.cpp

---
+++ HWW3.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int maxx = 10000;
+vector<int> g[maxx];
+vector<int> match1, match2;
+vector<bool> used;
+
+bool dfs(int v) {
+    if (used[v]) {
+        return false;
+    }
+    used[v] = true;
+    for (int to : g[v]) {
+        if (match2[to] == -1 or dfs(match2[to])) {
+            match1[v] = to;
+            match2[to] = v;
+            return true;
+        }
+    }
+    return false;
+}
+
+int kuhn(int n, int m) {
+    match1.assign(n, -1);
+    match2.assign(m, -1);
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        used.assign(n, false);
+        if (dfs(i)) {
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    int n, m, edges;
+    cin >> n >> m >> edges; // Кол-во вершин в первой и второй доле, кол-во всевозможных сочетаний
+    for (int i = 0; i < edges; i++) {
+        int f, s;
+        cin >> f >> s;
+        g[f].push_back(s);
+    }
+
+    int max_match = kuhn(n, m);
+    cout << "Max matching: " << max_match << endl;
+}
